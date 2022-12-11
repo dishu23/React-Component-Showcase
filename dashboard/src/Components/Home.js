9ed33f0c78fb56526a574bcb129cee44ebfb2dee
dishu23 <deekshagupta55001@gmail.com>
2022-12-11 11:25:07 +0530

    abc

--- dashboard/src/Components/Home.js
+++ dashboard/src/Components/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home-page'>
+      <div className='hone-div'>
+        <p className='pt-5'>WELCOME</p>
+        <h1>React Components Showcase</h1>
+      </div>
+
+    </div>
   )
 }
 
